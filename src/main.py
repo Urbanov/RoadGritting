dd120f8eafe808ff72e50fc7ece11fbdc0050d1e
Mikolaj Umanski <umaan96@gmail.com>
2018-01-18 00:43:01 +0100

    First version of csv parse + edges creation

--- src/main.py
+++ src/main.py
@@ -1,1 +1,19 @@
-print("halko")
+import networkx as nx
+import csv
+
+edgeList = []
+
+with open('test.csv', 'r') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        edgeList.append([int(x) for x in row])
+
+Graph = nx.Graph()
+
+for edge in edgeList:
+    Graph.add_edge(edge[0], edge[1], object=edge[2])
+
+print(Graph.edges)
+
+
+
